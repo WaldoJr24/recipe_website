b1cea5985e037f08f05b4aaedf5107346f66a880
WaldoJr24 <128355790+WaldoJr24@users.noreply.github.com>
2024-05-07 10:03:01 +0200

    sql inj. changes

--- recipe/__init__.py
+++ recipe/__init__.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://recipeuser:Heute000@141.87.59.255/recipedb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://recipeuser:Heute000@192.168.178.29/recipedb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#app.config['SECRET_KEY'] = 'something'
+app.config['SECRET_KEY'] = 'something'
 
 db = SQLAlchemy(app)
 
